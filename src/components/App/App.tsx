6e65eea8bb8bc62c27615c4e1704d04cd5900747
Martin Petersen <Fader4ever@hotmail.com>
2018-10-30 10:40:15 +0100

    Added project

---
+++ src/components/App/App.tsx
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import styles from './App.module.scss';
+
+class App extends Component {
+  render() {
+
+    return (
+      <div className={styles.AppHeader}>
+        
+        Content
+      </div>
+    );
+  }
+}
+
+export default App;
