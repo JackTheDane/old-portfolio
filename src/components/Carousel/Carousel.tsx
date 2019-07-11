2297d74bedc45a29d97b99de0ebe5e97c39787ce
Martin Petersen <Fader4ever@hotmail.com>
2019-07-11 23:00:57 +0200

    Basic Swiper integration

--- src/components/Carousel/Carousel.tsx
+++ src/components/Carousel/Carousel.tsx
@@ -1,42 +1,68 @@
 import React from 'react';
 import styles from './Carousel.module.scss';
+import Swiper from 'react-id-swiper';
 
 export interface CarouselProps {
   images: string[];
   className?: string;
 }
- 
-export interface CarouselState {
-  currentIndex: number;
-}
- 
-class Carousel extends React.Component<CarouselProps, CarouselState> {
-  constructor(props: CarouselProps) {
-    super(props);
-    this.state = {
-      currentIndex: 0
+
+class Carousel extends React.Component<CarouselProps> {
+
+  public render() {
+
+    const {
+      className,
+      images
+    } = this.props;
+
+    const params = {
+      containerClass: `swiper-container ${styles.carousel} ${className} card my-4`,
+      loop: true,
+      pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+        bulletActiveClass: styles.slideIndexButtonActive,
+        renderBullet: (idx: number, clsName: string) => `
+          <button
+            class="${styles.slideIndexButton} ${clsName ? clsName : ''}"}
+          </button>
+        `
+      }
     }
-  }
 
-  render() { 
     return (
-      <div className={`${styles.carousel} ${this.props.className} card my-4`}>
+      <Swiper {...params}>
+        {
+          images.map(
+            (image, i: number) => (
+              <div
+                key={'CarImg_' + i}
+                className={styles['carousel__image']}
+                style={{ backgroundImage: `url(${image})` }}></div>
+            )
+          )
+        }
+      </Swiper>
 
-        {this.getImages()}
+      /* <button onClick={() => { this.changeSlide(-1) }} className={`btn ${styles['carousel__navigation-button']} ${styles['carousel__navigation-button--left']}`}>
+        <i className="icon icon-arrow-left"></i>
+      </button>
+      <button onClick={() => { this.changeSlide(1) }} className={`btn ${styles['carousel__navigation-button']} ${styles['carousel__navigation-button--right']}`}>
+        <i className="icon icon-arrow-right"></i>
+      </button>
 
-        <button onClick={ () => {this.changeSlide(-1)}} className={`btn ${styles['carousel__navigation-button']} ${styles['carousel__navigation-button--left']}`}>
-          <i className="icon icon-arrow-left"></i>
-        </button>
-        <button onClick={ () => {this.changeSlide(1)}} className={`btn ${styles['carousel__navigation-button']} ${styles['carousel__navigation-button--right']}`}>
-          <i className="icon icon-arrow-right"></i>
-        </button>
-
-        <div className={styles.slideIndexButtonWrapper}>
-          { this.getSlideButtons() }
-        </div>
-
-
-      </div>
+      <div className={styles.slideIndexButtonWrapper}>
+        {
+          images.map(
+            (image: string, idx: number) => <button
+              key={`btn-${idx}`}
+              className={`${styles.slideIndexButton} ${currentIndex == idx ? styles.slideIndexButtonActive : ''}`}
+              onClick={() => this.setCurrentIndex(idx)}>
+            </button>
+          )
+        }
+      </div> */
     );
   }
 
@@ -46,49 +72,9 @@ class Carousel extends React.Component<CarouselProps, CarouselState> {
     })
   }
 
-  private changeSlide(value: number) {
-    const oldValue = this.state.currentIndex;
-    const newValue = oldValue + (value);
-    const imageArrayLength = this.props.images.length;
-    const lastImageIndex = imageArrayLength - 1;
+  // ---- Carousel interactions ---- //
 
-    let valueToUse: number;
 
-    if ( newValue >= 0 && newValue <= lastImageIndex ) {
-      valueToUse = newValue;
-    } else if ( newValue < 0 ) {
-      valueToUse = imageArrayLength - 1
-    } else {
-      valueToUse = 0;
-    }
-
-    this.setCurrentIndex(valueToUse)
-  }
-  
-  private setCurrentIndex(indexToSet: number) {
-    if (this.state.currentIndex !== indexToSet) {
-      this.setState({
-        currentIndex: indexToSet
-      });
-    }
-  }
-
-  private getImages() {
-    return this.props.images.map( (image, i: number) => <div 
-    key={'CarImg_'+i}
-    className={`${styles['carousel__image']} ${this.state.currentIndex === i ? styles['carousel__image--active'] : ''}`} 
-    style={{backgroundImage: `url(${ image })`}}></div> )
-  }
-
-  private getSlideButtons() {
-    return this.props.images.map(
-      (image: string, idx: number) => <button
-          key={`btn-${idx}`} 
-          className={`${styles.slideIndexButton} ${this.state.currentIndex == idx ? styles.slideIndexButtonActive : ''}`} 
-          onClick={() =>this.setCurrentIndex(idx)}>
-        </button>
-    )
-  }
 }
- 
+
 export default Carousel;
